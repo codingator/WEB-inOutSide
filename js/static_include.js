ce683b864156ad7f7eda8ea4e8b1f81548acbf0c
codingator <dev.jaehyeo.lee@gmail.com>
2020-05-12 23:04:45 +0900

    include test4

--- js/static_include.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  document.getElementById("nav_inc").innerHTML =
-    '<object type="text/html" data="nav_inc.html"></object>';
-  document.getElementById("footer_inc").innerHTML =
-    '<object type="text/html" data="footer_inc.html"></object>';
-});
